fe81b1c62cae06ed9c787e007a7195285c469ed4
Maria Emanuilova <mariyakoseva@yahoo.com>
2023-01-20 12:55:46 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum is equal to "+Summator.Sum(new int[] { 1,2,3}));
+            Summator.Test_SumTwoNumbers();
+        }
+    }
+}
