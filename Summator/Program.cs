273a5302c95909e62d87579d2e87c8715e107fe7
Maria Emanuilova <mariyakoseva@yahoo.com>
2023-01-20 15:52:33 +0200

    Add new method for average numbers, call it in the main class

--- Summator/Program.cs
+++ Summator/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("The sum is equal to "+Summator.Sum(new int[] { 1,2,3}));
+            Console.WriteLine("The average of the array numbers is: "+ Summator.Average(new int[] { 5,5,6}));
             
         }
     }
