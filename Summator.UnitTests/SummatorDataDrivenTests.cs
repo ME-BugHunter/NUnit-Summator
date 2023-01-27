f4168c1798e4ddaccc79da417251153bc32c3dd6
Maria Emanuilova <mariyakoseva@yahoo.com>
2023-01-27 21:07:57 +0200

    Added data driven tests for Summator - Sum method.

---
+++ Summator.UnitTests/SummatorDataDrivenTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.ComponentModel.DataAnnotations;
+
+namespace Summator.UnitTests
+{
+    public class SummatorDataDrivenTests
+    {
+        [TestCase(new int[] {1,2}, 3)]
+        [TestCase(new int[] {1}, 1)]
+        [TestCase(new int[] { -3, -9, -20 }, -32)]
+        [TestCase(new int[] { }, 0)]
+        [TestCase(new int[] { 2000000000, 2000000000, 2000000000, 2000000000 }, 8000000000)]
+        public void Test_Summator_SumDDT(int[] values, long expected)
+        {
+            var actual = Summator.Sum(values);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
